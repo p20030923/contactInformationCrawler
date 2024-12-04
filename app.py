f8c4d751ed655633a2ce44803c0d9dd7584923a7
劉松富 <3b117011@gm.student.ncut.edu.tw>
2024-12-04 21:04:46 +0800

    配對校正

--- app.py
+++ app.py
@@ -20,7 +20,7 @@ def insert_text() -> None:
     else:
         content = f"{'姓名':{chr(12288)}<4}{'職稱':{chr(12288)}<16}Email" + "\n" + "-" * 60 + "\n"
         for i in range(len(name_list)):
-            content += f'{name_list[i]}\t{job_list[i]}\t\t\t\t{emails_list[i]}\n'
+            content += f'{name_list[i]}\t{title_list[i]}\t\t\t\t{email_list[i]}\n'
         scrolled_text.insert(tkinter.END, content)
 
 # 使用 with 來處理資料庫連接與建立
@@ -36,36 +36,32 @@ with connect_db() as conn:
 URL = "https://csie.ncut.edu.tw/content.php?key=86OP82WJQO"
 
 response = requests.get(URL)
-# 描述 Email 的正規表示式物件 pattern
-pattern = re.compile(r'\b[\w.%+-]+@[A-Za-z0-9.-]+\.[A-Za-z]{2,}\b')
-# pattern = re.compile(r'\b[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Z|a-z]{2,}\b')
-match_list = pattern.findall(response.text)  # 從網頁原始碼中尋找所有 Email
-seen = set() # 由 set 確認去除重複的 Email 且不改變順序
-emails_list = []
-for item in match_list:
-    if item not in seen:
-        emails_list.append(item)
-        seen.add(item)
+# 設定允許空資料的re並將三個目標分組匹配
+# 因不明原因 三條re合併分組時抓不到資料 在Chat GPT建議下使用re.DOTALL
+pattern = re.compile(
+    r'<div class="member_name"><a href="[^"]+">([^<]*)</a>.*?'  # 姓名
+    r'<div class="member_info_title"><i class="fas fa-briefcase"></i>職稱</div>\s*<div class="member_info_content">([^<]*)</div>.*?'  # 職稱
+    r'<div class="member_info_title"><i class="fas fa-envelope"></i>信箱</div>\s*<div class="member_info_content"><a href="mailto:[^"]*">([^<]*)</a>',  # 信箱
+    re.DOTALL
+)
+match_list = re.findall(pattern, response.text)
 
-# 描述姓名的正規表示式物件 pattern
-pattern = re.compile(r'<div class="member_name"><a href="[^"]+">([^<]+)</a>')
-match_list = pattern.findall(response.text)  # 從網頁原始碼中尋找所有姓名
-name_list = match_list
-name_list = [item.strip() for item in name_list]
-
-# 描述職稱的正規表示式物件 pattern
-pattern = re.compile(r'<div class="member_info_content">([^<]+)</div>')
-match_list = pattern.findall(response.text)  # 從網頁原始碼中尋找所有職稱
-group_size = 4  # 去除多餘資料
-job_list = [match_list[i] for i in range(0, len(match_list), group_size)]
+# 重新排列以便存取
+name_list = []
+title_list = []
+email_list = []
+for i in match_list:
+    name_list.append(i[0])
+    title_list.append(i[1])
+    email_list.append(i[2])
 
 for i in range(len(name_list)):
-    print(f'{name_list[i]} {job_list[i]} {emails_list[i]}')
+    print(f'{name_list[i]} {title_list[i]} {email_list[i]}')
     # 使用 with 來處理資料庫連接與寫入
     with connect_db() as conn:
         cursor = conn.cursor()
         try:
-            cursor.execute("INSERT INTO contacts (name, title, email) VALUES (?, ?, ?)", (name_list[i], job_list[i], emails_list[i]))
+            cursor.execute("INSERT INTO contacts (name, title, email) VALUES (?, ?, ?)", (name_list[i], title_list[i], email_list[i]))
         except sqlite3.DatabaseError as e:
             print(f"資料庫操作發生錯誤: {e}")
         except Exception as e:
